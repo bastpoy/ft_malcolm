5b8197b968876a9450d6ce40468bf76951bae0ea
bastpoy <bastien.poyet@outlook.fr>
2024-09-24 14:18:13 +0200

    retrieve of mac address

--- malcolm.c
+++ malcolm.c
@@ -1,15 +1,3 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   malcolm.c                                          :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: bastpoy <bastpoy@student.42.fr>            +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/03/14 18:06:16 by bpoyet            #+#    #+#             */
-/*   Updated: 2024/09/20 21:33:26 by bastpoy          ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
@@ -18,67 +6,119 @@
 #include <stdio.h>
 #include <arpa/inet.h>
 #include <unistd.h>
+#include <ifaddrs.h>
+#include <linux/if_packet.h>
 
-#define PORT 3000
+#define PORT 219
 
-int main(void) 
+int fill_addr(struct sockaddr_in *targetaddr, char *addr)
 {
-    // declarer la structure
-    // socket
-    //bind
-    //listen
-    //accept
-    struct sockaddr_in myaddr;
-    struct sockaddr_in theiraddr;
-    socklen_t sin_size ;
+    int rval = 0;
 
-    int sockfd, new_fd;
-    int numbytes;
+    (*targetaddr).sin_port = htons(PORT);
+    (*targetaddr).sin_family = AF_INET;
+    if(!(rval = inet_pton(AF_INET, addr, &targetaddr->sin_addr.s_addr)))
+    {
+        printf("invalide address: %s\n", addr);
+        return(1);
+    }
+    else if(rval == -1)
+    {
+        printf("Error translating target address: %s\n",strerror(errno));
+        return(errno);
+    }
+    memset(&(targetaddr->sin_zero), '\0', 8);
+    return(0);
+}
+
+int main(int argc, char *argv[]) 
+{
+    (void) argc;
+    (void) argv;
+    struct sockaddr_in rec;
+    struct sockaddr_in *target;
+    struct ifaddrs *ifaddr, *ifa ;
+    int sockfd;
+    int broadcast = 1;
     char buffer[1024];
+    // socklen_t sin_size ;
     
-    sockfd = socket(PF_INET, SOCK_STREAM, 0);
+    // sin_size = sizeof(struct sockaddr);
+    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    // fill_addr(&targetaddr, argv[1]);
+    // fill_addr(&srcaddr, argv[2]);
 
-    myaddr.sin_port = htons(PORT);
-    myaddr.sin_family = AF_INET;
-    myaddr.sin_addr.s_addr = INADDR_ANY;
-    memset(&(myaddr.sin_zero), '\0', 8);
-
-    printf("my address is %s\n",  inet_ntoa(myaddr.sin_addr));
-
-    if(bind(sockfd, (struct sockaddr *)&myaddr, sizeof(struct sockaddr)) == -1)
+    if(setsockopt(sockfd, SOL_SOCKET, SO_BROADCAST, &broadcast, sizeof(broadcast)) == -1)
     {
-        perror("Bind error:");
+        printf("error setting brodcast address: %s", strerror(errno));
         return(errno);
     }
-    printf("Bind ok!\n");
-    if(listen(sockfd, 10) == -1)
+    int reuse = 1;
+    if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) == -1) {
+        perror("setsockopt(SO_REUSEADDR)");
+        close(sockfd);
+        return 1;
+    }
+
+    memset(&rec, 0, sizeof(rec));
+    rec.sin_family = AF_INET;
+    rec.sin_port = htons(PORT);
+    rec.sin_addr.s_addr = INADDR_ANY;
+
+    if (bind(sockfd, (struct sockaddr *)&rec, sizeof(rec)) == -1) {
+        perror("bind");
+        close(sockfd);
+        return (errno);
+    }
+
+    if(getifaddrs(&ifaddr) == -1)
     {
-        perror("Listen error: ");
+        printf("error on getting addr: %s\n", strerror(errno));
         return(errno);
     }
-    printf("Listen ok!\n");
-    while(1)
+    for(ifa = ifaddr; ifa; ifa = ifa->ifa_next)
     {
-        sin_size = sizeof(struct sockaddr_in);
-        if((new_fd = accept(sockfd, (struct sockaddr *)&theiraddr, &sin_size)) == -1)
+        if(ifa->ifa_addr && ifa->ifa_addr->sa_family == AF_INET)
         {
-            perror("error accept:");
-            return(1);
+            target = (struct sockaddr_in*)ifa->ifa_addr;
+            inet_ntop(AF_INET, &target->sin_addr, buffer, 1024);
+            printf("Interface : %s \t Address: %s \t MAC: %s\n", ifa->ifa_name, buffer, ifa->ifa_netmask->sa_data);
         }
-        printf("the address is: %s\n", inet_ntoa(theiraddr.sin_addr));
-        printf("Connection from: %s\n", inet_ntoa(theiraddr.sin_addr));
-
-        numbytes = recv(new_fd, buffer, sizeof(buffer)-1, 0);
-        if(numbytes == -1)
-        {
-            perror("recv error:");
-            return(1);
+        if (ifa->ifa_addr->sa_family == AF_PACKET && ifa->ifa_data != NULL) {
+            struct sockaddr_ll *s = (struct sockaddr_ll *)ifa->ifa_addr;
+            printf("Interface : %s \t MAC: ", ifa->ifa_name);
+            for (int i = 0; i < s->sll_halen; i++) {
+                printf("%02x%s", (unsigned char)s->sll_addr[i], (i + 1 != s->sll_halen) ? ":" : "");
+            }
+            printf("\n");
         }
-
-        buffer[numbytes] = '\0';  // Null-terminate the received data
-        printf("Received: %s\n", buffer);
-        close(new_fd);
     }
+    freeifaddrs(ifaddr);
+    // printf("listening...\n");
+    // while(1)
+    // {
+    //     int len = recvfrom(sockfd, buffer, sizeof(buffer), 0, (struct sockaddr *)&target, &sin_size);
+    //     if(len == -1)
+    //     {
+    //         printf("Error recvfrom: %s\n", strerror(errno));
+    //         return(errno);
+    //     }
+    //     else
+    //     {
+    //         buffer[len] = '\0';
+    //         printf("receiving messages: %s\n", buffer);
+    //     }
+    //     int size = sendto(sockfd, "message received", 17, 0, (struct sockaddr *)&target, sizeof(target));
+    //     if(size < 0)
+    //     {
+    //         printf("big problem\n");
+    //     }
+    //     else
+    //     {
+    //         printf("Reply sent to %s:%d\n", inet_ntoa(target.sin_addr), ntohs(target.sin_port));
+    //     }
+    // }
     close(sockfd);
+    return(0);
 }
 
--- readme.md
+++ readme.md
@@ -3,20 +3,38 @@
 ## EXPLANATION
 
 The goal of this project is to implement man in the middle attack with the langage C.
-There is a program who will listen for incoming request source ip address. And then it will send a ARP message to the target ip address. then the routing table of the target IP address will be update with the fraudulent ip adress and not the real source IP address.
+There is a program who will listen for incoming request on the broadcast by the target IP address requesting the source IP address. The program will send an ARP response to the target. At the end of the program the ARP table of the target should contain the IP source address
 
-TCP: 
-- protocole utilise pour naviguer sur internet (HTTP/HTTPS) aussi pour le SMTP avec une gestion des paquets.
-- Mise en place de la communication avec l'envoie de paquet au prealable pour etablir la communication
-- le TCP est dit connecte
+## LAUNCH THE PROGRAMM
 
-UDP
-- protocole sans gestion des paquets. Protocole plus rapide que le TCP et permet de charger un flux video par exemple
-- Pas dacquittement lors de l'envoie et de la reception des paquets
-- l'UDP est dit non connecte
+./malcolm <ip_source> <mac_source> <ip_target> <mac_target>
 
-SOCKET
-- un socket est le moyen de communiquer entre un client et un serveur generalement. Il offre des informations sur le client ou le serveur a contacter (adresse IP)
+## FUNCTIONS AUTORISEES
+
+- sendto(): send data over a socket
+    - ssize_t sendto(int sockfd, const void *buf, size_t len, int flags, const struct sockaddr *dest_addr, socklen_t addrlen);
+    - principalement utilise en connectionless socket, ou chaque paquet est independant les uns des autrese (UDP)
+- recvfrom(): l'inverse de sendto et permet de recevoir un packet
+- socket: permet de creer un socket
+    - preciser le type d'adresse
+    - le type de communication (TCP, UDP)
+    - protocole : un protocole specifique a mettre en oeuvre (peut ne pas en avoir)
+- setsocketopt(): permet de specifier des options liees au fd du socket
+- if_nametoindex(): permet de recuperer l'index d'une interface reseau grace a son index
+    - elle retourne donc un index
+    - elle prend en parametre une chaine de caractere correspondant au nom de l'interface
+- getuid():récupérer l'id de l'utilisateur
+- signal et sigaction sont des fonctions qui permettent de gerer les signaux de retour d'une action.
+- gethostbyname: recuperer des informations sur un domaine en envoyant un URL
+    - Un peu dépassé car cette fonction ne prend pas en compte les addresses IPV6
+- getaddreinfo: Prend en argument le nom de domaine a resoudre et des structures pour stocker le résultat.
+- freeaddrinfo: libère la mémoire allouée par getaddrinfo
+- getifaddrs: récupérer les interfaces réseaux disponibles sur une machine
+    - Elle prend en paramètre un vers un pointeur de structure ou va être stocker le résultat
+- freeifaddrs: free le pointeur alloué par getifaddrs
+- htons et ntohs: convertisse les octet de l'hote vers l'octet du réseau.
+    - Parce que l'ordre d'octets du réseau est normé.
+    - Enfaite ce qu'il va faire c'est convertir en inversant le sens des bits
 
 ## STRUCTURES
 
@@ -51,36 +69,19 @@ struct hostent {
    char   **h_addr_list;  /* Liste d'adresses.         */
 }
 
-## NOTIONS RESEAUX
+## NETWORK NOTIONS
+
+### Network specifications
 
 - wlp2s0 => interface reseau qui correspond au WIFI
 - lo => interface reseau virtuelle qui correspond a la loopback pour les tests et la communications internes
     il a une adresse IP egale a 127.0.0.1
 
+- PORT ARP : 219
 
-## FUNCTIONS AUTORISEES
+### Bash command to communicate
 
-- sendto(): send data over a socket
-    - ssize_t sendto(int sockfd, const void *buf, size_t len, int flags, const struct sockaddr *dest_addr, socklen_t addrlen);
-    - principalement utilise en connectionless socket, ou chaque paquet est independant les uns des autrese (UDP)
-- recvfrom(): l'inverse de sendto et permet de recevoir un packet
-- socket: permet de creer un socket
-    - preciser le type d'adresse
-    - le type de communication (TCP, UDP)
-    - protocole : un protocole specifique a mettre en oeuvre (peut ne pas en avoir)
-- setsocketopt(): permet de specifier des options liees au fd du socket
-- if_nametoindex(): permet de recuperer l'index d'une interface reseau grace a son index
-    - elle retourne donc un index
-    - elle prend en parametre une chaine de caractere correspondant au nom de l'interface
-- getuid():récupérer l'id de l'utilisateur
-- signal et sigaction sont des fonctions qui permettent de gerer les signaux de retour d'une action.
-- gethostbyname: recuperer des informations sur un domaine en envoyant un URL
-    - Un peu dépassé car cette fonction ne prend pas en compte les addresses IPV6
-- getaddreinfo: Prend en argument le nom de domaine a resoudre et des structures pour stocker le résultat.
-- freeaddrinfo: libère la mémoire allouée par getaddrinfo
-- getifaddrs: récupérer les interfaces réseaux disponibles sur une machine
-    - Elle prend en paramètre un vers un pointeur de structure ou va être stocker le résultat
-- freeifaddrs: free le pointeur alloué par getifaddrs
-- htons et ntohs: convertisse les octet de l'hote vers l'octet du réseau.
-    - Parce que l'ordre d'octets du réseau est normé.
-    - Enfaite ce qu'il va faire c'est convertir en inversant le sens des bits
+- nc : listen and connection with TCP and UDP (netcat)
+    - nc <ip_address> <port>
+    - echo "<message>" | nc <ip_address> <port>
+- arp -a : get the routing table of a machine
